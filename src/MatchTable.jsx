102acac57a44ab2dad29fcb22197a210a5b97441
naoki114 <tekisandemitry>
2016-12-04 02:32:00 +0900

    [fix] Undefinedに対してgetしようとしてエラー吐いていたので修正

--- src/MatchTable.jsx
+++ src/MatchTable.jsx
@@ -4,15 +4,43 @@ import dummyMatchResults from './dummyMatchResults';
 export default class MatchTable extends React.PureComponent {
 	
 	/**
+	 * 対戦表のセルを描画
+	 * @param matchResult 対戦成績
+	 * @param leftPlayerId 左側に表示するポイントのプレイヤーId
+	 * @param rightPlayerId 右側に表示するポイントのプレイヤーId
+	 */
+	renderCell (matchResult, leftPlayerId, rightPlayerId) {
+		const leftPlayerPoint = matchResult.getIn([leftPlayerId, 'point']);
+		const rightPlayerPoint = matchResult.getIn([rightPlayerId, 'point']);
+		const resultString = [leftPlayerPoint, rightPlayerPoint].join('-');
+		return (
+			<div className="matchResultCell">
+				{resultString}
+			</div>  
+
+		);
+	}
+
+	/**
 	 *　対戦表の行を描画
 	　*/
-	renderRow(playerId, playerMap, matchResultList){
+	renderRow(playerId, playerIdList, playerMap, matchResultMap){
 		const playerName = playerMap.getIn([playerId, 'name']);
-		const matchResultIdMap = playerMap.getIn([playerId, 'matchResultIdMap'])
-		const matchResult = matchResultList.get
+		const matchResultIdMap = playerMap.getIn([playerId, 'matchResultIdMap']);
 		return (
 			<div className="row">
-			 	{matchResultIdMap}
+			 	{
+			 		playerIdList.map((innerPlayerId) => {
+			 			if(innerPlayerId !== playerId) {
+				 			const matchResultId = matchResultIdMap.get(innerPlayerId);
+				 			console.log(matchResultIdMap.toJS());
+				 			console.log(innerPlayerId);
+				 			const matchResult = matchResultMap.get(matchResultId);
+				 			return this.renderCell(matchResult, playerId, innerPlayerId);
+			 			}
+			 			return null;
+			 		})
+			 	}
 			</div>
 		);
 	}
@@ -21,14 +49,15 @@ export default class MatchTable extends React.PureComponent {
 	render() {
 		const matchResults = dummyMatchResults;
 		const players = matchResults.get('players');
+		const playerIdList = players.get('idList');
 		const playerMap =  players.get('byId');
-		const matchResultList = matchResults.get('matchResults');
+		const matchResultMap = matchResults.getIn(['matchResults', 'byId']);
 		return (
 			<div className="matchTable">
 				{
 					// 行を生成
-					players.get('idList').map((playerId) => {
-						return renderRow(playerId, playerMap, matchResultList);
+					playerIdList.map((playerId) => {
+						return this.renderRow(playerId, playerIdList, playerMap, matchResultMap);
 					})
 				}			
 			</div>
